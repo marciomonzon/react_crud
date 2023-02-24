31a903598400cf5672a40df31d4f8e98de2a1abd
marciomonzon <marcio.pcmonzon@gmail.com>
2023-02-24 19:25:43 -0300

    layoutbase

---
+++ crud-material/src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,9 @@
+import { LayoutBaseDePagina } from "../../shared/layouts";
+
+export const Dashboard = () => {
+    return (
+        <LayoutBaseDePagina titulo='Pagina Inicial'>
+            Testando
+        </LayoutBaseDePagina>
+    );
+};
--- crud-material/src/routes/index.tsx
+++ crud-material/src/routes/index.tsx
@@ -1,16 +1,27 @@
-import { Button } from '@mui/material';
+import { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
-import { useAppThemeContext, useDrawerContext } from '../shared/contexts';
+import { useDrawerContext } from '../shared/contexts';
+import { Dashboard } from '../pages';
 
 export const AppRoutes = () => {
-  const { toggleDrawerOpen } = useDrawerContext();
+  const {  setDrawerOptions } = useDrawerContext();
+
+  useEffect(() => {
+    setDrawerOptions([
+      {
+        icon: 'home',
+        path: '/pagina-inicial',
+        label: 'Página inicial',
+      }
+    ]);
+  }, []);
 
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<Button variant='contained' color='primary' onClick={toggleDrawerOpen}>Toggle theme</Button>} />
+      <Route path="/pagina-inicial" element={<Dashboard />} />
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
   );
-}
+};
