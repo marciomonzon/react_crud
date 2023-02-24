b5f0849f9ac32633f22f30cfa387c4fcdfdf197e
marciomonzon <marcio.pcmonzon@gmail.com>
2023-02-24 18:09:28 -0300

    drawer responsive

--- crud-material/src/App.tsx
+++ crud-material/src/App.tsx
@@ -3,17 +3,19 @@ import { ThemeProvider } from "@mui/system";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
 import { MenuLateral } from "./shared/components";
-import { AppThemeProvider } from "./shared/contexts";
+import { AppThemeProvider, DrawerProvider } from "./shared/contexts";
 import { DarkTheme, LightTheme } from "./shared/themes";
 
 export const App = () => {
   return (
     <AppThemeProvider>
-      <BrowserRouter>
-      <MenuLateral>
-          <AppRoutes />
-      </MenuLateral>
-      </BrowserRouter>
-    </AppThemeProvider>
-  );
+      <DrawerProvider>
+        <BrowserRouter>
+        <MenuLateral>
+            <AppRoutes />
+        </MenuLateral>
+        </BrowserRouter>
+      </DrawerProvider>
+      </AppThemeProvider>
+    );
 }
