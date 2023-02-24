e6f161f658d62bb7094f7fbd4faf263b137be6e2
marciomonzon <marcio.pcmonzon@gmail.com>
2023-02-24 15:05:50 -0300

    theme

--- crud-material/src/App.tsx
+++ crud-material/src/App.tsx
@@ -2,14 +2,15 @@
 import { ThemeProvider } from "@mui/system";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
-import { LightTheme } from "./shared/themes";
+import { AppThemeProvider } from "./shared/contexts";
+import { DarkTheme, LightTheme } from "./shared/themes";
 
 export const App = () => {
   return (
-    <ThemeProvider theme={LightTheme}>
+    <AppThemeProvider>
       <BrowserRouter>
         <AppRoutes />
       </BrowserRouter>
-    </ThemeProvider>
+    </AppThemeProvider>
   );
 }
--- crud-material/src/routes/index.tsx
+++ crud-material/src/routes/index.tsx
@@ -1,11 +1,16 @@
 import { Button } from '@mui/material';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
+import { useAppThemeContext } from '../shared/contexts';
+
 export const AppRoutes = () => {
-    return (
-        <Routes>
-            <Route path="/pagina-inicial" element={<Button variant='contained' color='primary'>Teste</Button>} />
-            <Route path="*" element={<Navigate to="/pagina-inicial" />}/>
-        </Routes>
-    );
+  const { toggleTheme } = useAppThemeContext();
+
+  return (
+    <Routes>
+      <Route path="/pagina-inicial" element={<Button variant='contained' color='primary' onClick={toggleTheme}>Toggle theme</Button>} />
+
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+    </Routes>
+  );
 }
