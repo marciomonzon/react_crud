4b60cb5aaa41dd7af72a331b08de6b4e27c50fc7
marciomonzon <marcio.pcmonzon@gmail.com>
2023-02-24 17:14:12 -0300

    drawer menu

--- crud-material/src/App.tsx
+++ crud-material/src/App.tsx
@@ -2,6 +2,7 @@
 import { ThemeProvider } from "@mui/system";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
+import { MenuLateral } from "./shared/components";
 import { AppThemeProvider } from "./shared/contexts";
 import { DarkTheme, LightTheme } from "./shared/themes";
 
@@ -9,7 +10,9 @@ export const App = () => {
   return (
     <AppThemeProvider>
       <BrowserRouter>
-        <AppRoutes />
+      <MenuLateral>
+          <AppRoutes />
+      </MenuLateral>
       </BrowserRouter>
     </AppThemeProvider>
   );
